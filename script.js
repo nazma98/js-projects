82337e340670a2d38a343ccd54c7886fd0bd633f
nazma98 <nazmasarker1311@gmail.com>
2024-12-25 00:09:47 +0600

    Add addTask and addTaskList methods

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ const tasks = []
 const addTaskElement = document.getElementById('addTask');
 const taskInputElement = document.getElementById('taskInput');
 
+const addTask = () => {
+    const taskText = taskInputElement.value;
+    if(!taskText){
+        alert('You can\'t add empty task');
+    }
+    addTaskList(taskText);
+}
+
+const addTaskList = (taskText) => {
+    tasks.push(taskText);
+    renderTasks(tasks);
+}
+
 addTaskElement.addEventListener('click', addTask);
