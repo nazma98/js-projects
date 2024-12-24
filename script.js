349829ad1fe0e6afbf288beb2f150698b16e2b1d
nazma98 <nazmasarker1311@gmail.com>
2024-12-24 23:59:20 +0600

    Add eventlistener for add task button

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const tasks = []
 
 const addTaskElement = document.getElementById('addTask');
 const taskInputElement = document.getElementById('taskInput');
+
+addTaskElement.addEventListener('click', addTask);
